319ffd66a3dd15394bf873db83029ab7f0059f45
Nick <kosmynin84@outlook.com>
2021-08-18 12:07:28 +0300

    accordion--create--plus

--- src/js/index.js
+++ src/js/index.js
@@ -1,23 +1,36 @@
 window.addEventListener('DOMContentLoaded', function () {
 
     // const btnPlus = document.querySelector('.services__plus'),
-    const    btnCross = document.querySelector('.services__cross'),
-        accordionPanel = document.querySelector('.services__button');
+    // const    btnCross = document.querySelector('.services__cross'),
+    //     accordionPanel = document.querySelector('.services__button');
 
-    btnCross.style.display = 'none';
+    // btnCross.style.display = 'none';
 
 
-    
-    accordionPanel.addEventListener('click', function (btnPlus) {
-        
-        if (btnPlus.style.display == 'block') {
-            btnPlus.style.display = 'none';
+
+    // accordionPanel.addEventListener('click', function (btnPlus) {
+
+    //     if (btnPlus.style.display == 'block') {
+    //         btnPlus.style.display = 'none';
+    //     }
+    // });
+
+
+    const accordionPanel = document.querySelector('.services__button'),
+        servisePlus = document.createElement('div');
+
+    servisePlus.className = 'service__plus';
+    servisePlus.innerHTML = '';
+    servisePlus.classList.add('show');
+    accordionPanel.appendChild(servisePlus);
+
+    accordionPanel.addEventListener('click',function(){
+        if(servisePlus.classList.contains('show')){
+            servisePlus.classList.remove('show');
+            servisePlus.classList.add('hide');
+        }else{
+            servisePlus.classList.remove('hide');
+            servisePlus.classList.add('show');
         }
     });
-
-    
-
-
-
-
 });
