7886ce1a5d152d9a70e8698b6c30d362e5e45387
Nick <kosmynin84@outlook.com>
2021-08-18 18:14:46 +0300

    accordion--plus 1

--- src/js/index.js
+++ src/js/index.js
@@ -1,36 +1,39 @@
 window.addEventListener('DOMContentLoaded', function () {
 
-    // const btnPlus = document.querySelector('.services__plus'),
-    // const    btnCross = document.querySelector('.services__cross'),
-    //     accordionPanel = document.querySelector('.services__button');
-
-    // btnCross.style.display = 'none';
-
-
-
-    // accordionPanel.addEventListener('click', function (btnPlus) {
-
-    //     if (btnPlus.style.display == 'block') {
-    //         btnPlus.style.display = 'none';
-    //     }
-    // });
-
-
     const accordionPanel = document.querySelector('.services__button'),
-        servisePlus = document.createElement('div');
+        servisePlus = document.createElement('div'),
+        serviceCross = document.querySelector('.services__cross'),
+        serviceWindow =document.querySelector('.services__panel');
+
+
 
     servisePlus.className = 'service__plus';
     servisePlus.innerHTML = '';
     servisePlus.classList.add('show');
     accordionPanel.appendChild(servisePlus);
+    serviceCross.classList.add('hide');
 
-    accordionPanel.addEventListener('click',function(){
-        if(servisePlus.classList.contains('show')){
+    accordionPanel.addEventListener('click', function () {
+        if (servisePlus.classList.contains('show')) {
+            // hide plus
             servisePlus.classList.remove('show');
             servisePlus.classList.add('hide');
-        }else{
+            // show cross
+            serviceCross.classList.remove('hide');
+            serviceCross.classList.add('showCross');
+            // show service-window
+            serviceWindow.classList.add('showCross');
+
+        } else {
+            // toggle plus 
             servisePlus.classList.remove('hide');
             servisePlus.classList.add('show');
+            // hide cross
+            serviceCross.classList.remove('showCross');
+            serviceCross.classList.add('hide');
+            // hide service-window
+            serviceWindow.classList.remove('showCross');
+            serviceWindow.classList.add('hide');
         }
     });
 });
